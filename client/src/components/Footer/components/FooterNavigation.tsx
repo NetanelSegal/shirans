2c9c077204c93f91dd8e798dfe896fef2a404be8
NetanelSegal <segal.netanel4@gmail.com>
2024-11-22 13:36:46 +0200

    deleted unused veriables

--- client/src/components/Footer/components/FooterNavigation.tsx
+++ client/src/components/Footer/components/FooterNavigation.tsx
@@ -2,26 +2,32 @@ import { appRoutes } from '@/App';
 import { Link } from 'react-router-dom';
 
 export default function FooterNavigation() {
+  const chunks = chunkArray(
+    appRoutes.filter(({ notNavigateable }) => !notNavigateable),
+    2,
+  );
   return (
     <div className='gap-5 text-white underline sm:mx-0 sm:flex sm:items-end md:pb-2'>
-      {appRoutes.map(
-        (route, index) =>
-          index % 3 === 0 && (
-            <div
-              key={index / 3}
-              className='flex justify-center gap-5 text-center sm:flex-col sm:gap-0'
-            >
-              {appRoutes.slice(index, index + 3).map(
-                ({ title, path, notNavigateable }) =>
-                  !notNavigateable && (
-                    <Link key={title} className='sm:text-start' to={path}>
-                      <p>{title}</p>
-                    </Link>
-                  ),
-              )}
-            </div>
-          ),
-      )}
+      {chunks.map((chunk, chunkIndex) => (
+        <div
+          key={chunkIndex}
+          className='flex justify-center gap-5 text-center sm:flex-col sm:gap-0'
+        >
+          {chunk.map(({ title, path }) => (
+            <Link key={title} className='sm:text-start' to={path}>
+              <p>{title}</p>
+            </Link>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
+
+const chunkArray = (array: any[], size: number) => {
+  const chunks = [];
+  for (let i = 0; i < array.length; i += size) {
+    chunks.push(array.slice(i, i + size));
+  }
+  return chunks;
+};
