196f1054e856ca9706d176cfe7eb164bca614952
netanel segal <segal.netanel4@gmail.com>
2025-12-30 20:53:31 +0200

    added styling for image optimization effect

--- client/src/components/ui/Image/Image.tsx
+++ client/src/components/ui/Image/Image.tsx
@@ -1,18 +1,42 @@
 import image404src from '@/assets/Image_404.svg';
-import { ImgHTMLAttributes, SyntheticEvent } from 'react';
+import { ImgHTMLAttributes, SyntheticEvent, useState } from 'react';
 
 export default function Image({
   ...rest
 }: ImgHTMLAttributes<HTMLImageElement>) {
-  return (
-    <img
-      loading='lazy'
-      {...rest}
-      onError={(e: SyntheticEvent<HTMLImageElement, Event>) => {
-        console.log((e.target as HTMLImageElement).src);
+  const [isLoading, setIsLoading] = useState(true);
 
-        (e.target as HTMLImageElement).src = image404src;
-      }}
-    />
+  const handleLoad = () => {
+    setIsLoading(false);
+  };
+  console.log('isLoading', isLoading, 'src', rest.src);
+  return (
+    <div
+      className='relative'
+      {...(isLoading
+        ? {
+            style: {
+              backgroundImage: `url(${rest.src})`,
+              backgroundSize: rest.style?.backgroundSize || 'cover',
+              backgroundPosition: rest.style?.backgroundPosition || 'center',
+              backgroundRepeat: rest.style?.backgroundRepeat || 'no-repeat',
+              backgroundColor: rest.style?.backgroundColor || 'transparent',
+            },
+          }
+        : {})}
+    >
+      <img
+        style={{
+          opacity: isLoading ? 0 : 1,
+          transition: 'opacity 0.3s ease-in-out',
+        }}
+        loading='lazy'
+        {...rest}
+        onError={(e: SyntheticEvent<HTMLImageElement, Event>) => {
+          (e.target as HTMLImageElement).src = image404src;
+        }}
+        onLoad={handleLoad}
+      />
+    </div>
   );
 }
