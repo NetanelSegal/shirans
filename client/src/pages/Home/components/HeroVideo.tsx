cc81ac5a1f306f3f954175eb9b3ebb0d390fa08e
netanel segal <segal.netanel4@gmail.com>
2026-02-18 14:42:31 +0200

    changed video in hero sections home page

---
+++ client/src/pages/Home/components/HeroVideo.tsx
@@ -0,0 +1,20 @@
+import videoDesktop from '@/assets/hero-vid-desktop.mov';
+import videoMobile from '@/assets/hero-vid-mobile.mov';
+import { useScreenContext } from '@/contexts/ScreenProvider';
+export default function HeroVideo() {
+    const { isSmallScreen } = useScreenContext();
+    const videoSrc = isSmallScreen ? videoMobile : videoDesktop;
+    return (
+        <div className='size-full overflow-hidden'>
+            <video
+                autoPlay
+                muted
+                loop
+                playsInline
+                className='size-full object-cover'
+            >
+                <source src={videoSrc} />
+            </video>
+        </div>
+    )
+}
