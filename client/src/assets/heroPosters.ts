f526503639315c2f3e983ea703b976b0621888c8
netanel segal <segal.netanel4@gmail.com>
2026-05-24 14:17:19 +0300

    up

---
+++ client/src/assets/heroPosters.ts
@@ -0,0 +1,7 @@
+import heroDesktopPoster from '../../public/assets/hero-vid-desktop.jpg';
+import heroMobilePoster from '../../public/assets/hero-vid-mobile.jpg';
+
+export const heroPosters = {
+  desktop: heroDesktopPoster,
+  mobile: heroMobilePoster,
+} as const;
